298b99b86c63fd06725d6b9cfe077cbf76571e4a
Mathias Hausleithner <hausleithnermathias@gmail.com>
2019-10-15 19:36:13 +0200

    added structure

---
+++ app/src/main/java/at/dkepr/cinemaservice/CinemaserviceApplication.java
@@ -0,0 +1,14 @@
+package at.dkepr.cinemaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaserviceApplication.class, args);
+		System.out.println("sers");
+	}
+
+}
