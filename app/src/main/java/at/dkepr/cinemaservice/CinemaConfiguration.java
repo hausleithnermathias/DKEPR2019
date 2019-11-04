8e6b49d4ab78fc2f62f29771fd8dfae4b18b0361
11701902 <k11701902@students.jku.at>
2019-11-04 20:51:48 +0100

    Beispiel RDF Model aus File erzeugen und durchiterieren (in FusekiServerTest)

--- app/src/main/java/at/dkepr/cinemaservice/CinemaConfiguration.java
+++ app/src/main/java/at/dkepr/cinemaservice/CinemaConfiguration.java
@@ -6,10 +6,12 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import javax.annotation.PostConstruct;
 
 @Configuration
-public class CinemaConfiguration implements ApplicationListener<ApplicationReadyEvent> {
+public class CinemaConfiguration  {
 
+    /*
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent applicationReadyEvent) {
         System.out.println("Hello!");
@@ -21,8 +23,14 @@ public class CinemaConfiguration implements ApplicationListener<ApplicationReady
         catch (Exception c) {
             c.printStackTrace();
         }
-
-         */
+        return;
 
     }
+    */
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("test");
+    }
+
 }
--- app/src/main/java/at/dkepr/cinemaservice/CinemaserviceApplication.java
+++ app/src/main/java/at/dkepr/cinemaservice/CinemaserviceApplication.java
@@ -1,6 +1,5 @@
 package at.dkepr.cinemaservice;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
