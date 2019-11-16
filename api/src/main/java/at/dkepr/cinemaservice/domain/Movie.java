c2e45a2bca0034d9da665054ef3fe98cf01ff44f
Mathias Hausleithner <hausleithnermathias@gmail.com>
2019-11-16 12:48:12 +0100

    added frontent

---
+++ api/src/main/java/at/dkepr/cinemaservice/domain/Movie.java
@@ -0,0 +1,93 @@
+package at.dkepr.cinemaservice.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Maps;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.apache.commons.compress.utils.Lists;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@ApiModel(value = "Kino", description = "Kino", discriminator = "loB")
+public class Movie {
+
+    public Movie() {
+        reservations = Maps.newHashMap();
+        reservations.put(WeekDayEnum.Mo, Lists.newArrayList());
+        reservations.put(WeekDayEnum.Di, Lists.newArrayList());
+        reservations.put(WeekDayEnum.Mi, Lists.newArrayList());
+        reservations.put(WeekDayEnum.Do, Lists.newArrayList());
+        reservations.put(WeekDayEnum.Fr, Lists.newArrayList());
+        reservations.put(WeekDayEnum.Sa, Lists.newArrayList());
+        reservations.put(WeekDayEnum.So, Lists.newArrayList());
+
+    }
+
+    @ApiModelProperty(required = false, value = "Filmtitel")
+    @JsonProperty("titel")
+    private String title;
+
+    @ApiModelProperty(required = false, value = "Altersfreigabe")
+    @JsonProperty("altersfreigabe")
+    private String ageRating;
+
+    @ApiModelProperty(required = false, value = "Sprache des Films")
+    @JsonProperty("sprache")
+    private String language;
+
+    @ApiModelProperty(required = false, value = "Dauer des Films")
+    @JsonProperty("spieldauer")
+    private String movieLength;
+
+    @ApiModelProperty(required = false, value = "Ort und Erscheinungsjahr")
+    @JsonProperty("country")
+    private String country;
+
+    @ApiModelProperty(required = false, value = "Genre")
+    @JsonProperty("genre")
+    private String genre;
+
+    @ApiModelProperty(required = false, value = "Tage, an denen der Film gespielt wird")
+    @JsonProperty("spieltage")
+    private List<WeekDayEnum> daysPlayed;
+
+    @ApiModelProperty(required = false, value = "Reservierungen pro Tag")
+    @JsonProperty("reservierungen")
+    private Map<WeekDayEnum, List<String>> reservations;
+
+    @ApiModelProperty(required = false, value = "Schauspieler")
+    @JsonProperty("schauspieler")
+    private List<String> actors;
+
+    @ApiModelProperty(required = false, value = "Filmproduzent")
+    @JsonProperty("filmproduzent")
+    private List<String> producer;
+
+    public List<WeekDayEnum> getDaysPlayed() {
+        if (daysPlayed == null) {
+            daysPlayed = Lists.newArrayList();
+        }
+        return daysPlayed;
+    }
+
+    public Map<WeekDayEnum, List<String>> getReservations() {
+        return reservations;
+    }
+
+    public List<String> getActors() {
+        if (actors == null) {
+            actors = Lists.newArrayList();
+        }
+        return actors;
+    }
+
+    public List<String> getProducer() {
+        if (producer == null) {
+            producer = Lists.newArrayList();
+        }
+        return producer;
+    }
+}
--- api/src/main/java/at/dkepr/cinemaservice/externalAPI/MetaServiceAPI.java
+++ api/src/main/java/at/dkepr/cinemaservice/externalAPI/MetaServiceAPI.java
@@ -1,12 +1,16 @@
 package at.dkepr.cinemaservice.externalAPI;
 
+import at.dkepr.cinemaservice.domain.Movie;
+import at.dkepr.cinemaservice.domain.WeekDayEnum;
+import org.apache.commons.compress.utils.Lists;
+import org.apache.jena.base.Sys;
 import org.apache.jena.rdf.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.*;
+import java.io.IOException;
+import java.util.List;
 
 
 @RestController
@@ -22,36 +26,82 @@ public class MetaServiceAPI {
     }
 
     @RequestMapping(value = "/Movies/{day}", method = RequestMethod.GET)
-    public @ResponseBody String getMoviesByDay(@PathVariable String day) throws IOException {
+    public @ResponseBody List<Movie> getMoviesByDay(@PathVariable String day) throws IOException {
 
         Model model = ModelFactory.createDefaultModel();
         model.read("http://localhost:8080/Cinema1/Movies/" + day);
 
+        List<Movie> movieList = Lists.newArrayList();
+        Movie movie = new Movie();
         // list the statements in the Model
         StmtIterator iter = model.listStatements();
+        org.apache.jena.rdf.model.Resource subject = null;
+        Property predicate;
+        RDFNode object;
         // print out the predicate, subject and object of each statement
         while (iter.hasNext()) {
             Statement stmt = iter.nextStatement();  // get next statement
-            org.apache.jena.rdf.model.Resource subject = stmt.getSubject();     // get the subject
-            Property predicate = stmt.getPredicate();   // get the predicate
-            RDFNode object = stmt.getObject();      // get the object
-
-            System.out.print(subject.toString());
-            System.out.print(" " + predicate.toString() + " ");
-            if (object instanceof Resource) {
-                System.out.print(object.toString());
-            } else {
-                // object is a literal
-                System.out.print(" \"" + object.toString() + "\"");
+            if(subject == null){
+                subject = stmt.getSubject();
             }
-            System.out.println(" .");
+            System.out.println(subject.getLocalName() + "  " + stmt.getSubject().getLocalName());
+            if(!subject.getLocalName().equals(stmt.getSubject().getLocalName())){
+                movieList.add(movie);
+                movie = new Movie();
+            }
+            subject = stmt.getSubject(); // get the subject
+            predicate = stmt.getPredicate();   // get the predicate
+            object = stmt.getObject();// get the object
+            String[] properties;
 
-
+            switch(predicate.getLocalName()){
+                case "actors":
+                    properties = object.toString().split(", ");
+                    for(int i = 0; i<properties.length; i++){
+                        movie.getActors().add(properties[i]);
+                    }
+                    break;
+                case "country":
+                    movie.setCountry(object.toString());
+                    break;
+                case "genre":
+                    movie.setGenre(object.toString());
+                    break;
+                case "duration":
+                    movie.setMovieLength(object.toString());
+                    break;
+                case "production":
+                    properties = object.toString().split(", ");
+                    for(int i = 0; i<properties.length; i++){
+                        movie.getProducer().add(properties[i]);
+                    }
+                    break;
+                case "language":
+                    movie.setLanguage(object.toString());
+                    break;
+                case "reservations":
+                    properties = object.toString().split(";");
+                    for(int i = 0; i<properties.length; i++){
+                        String[] subProperties = properties[i].split(":");
+                        movie.getReservations().get(WeekDayEnum.valueOf(subProperties[1])).add(subProperties[0]);
+                    }
+                    break;
+                case "days":
+                    properties = object.toString().split(";");
+                    for(int i = 0; i<properties.length; i++){
+                        movie.getDaysPlayed().add(WeekDayEnum.valueOf(properties[i]));
+                    }
+                    break;
+                case "agerating":
+                    movie.setAgeRating(object.toString());
+                    break;
+                case "title":
+                    movie.setTitle(object.toString());
+                    break;
+            }
         }
-        try(final ByteArrayOutputStream os = new ByteArrayOutputStream() ){
-            model.write(os);
-            return os.toString();
-        }
+        movieList.add(movie);
+        return movieList;
     }
 
 
