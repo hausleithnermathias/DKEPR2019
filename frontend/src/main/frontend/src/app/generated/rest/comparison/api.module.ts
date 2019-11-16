6376c2348a626a286ffe2186c14c2314bafa60d8
11701902 <k11701902@students.jku.at>
2019-11-16 14:30:52 +0100

    cinema1 changes

---
+++ frontend/src/main/frontend/src/app/generated/rest/comparison/api.module.ts
@@ -0,0 +1,37 @@
+import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import { Configuration } from './configuration';
+import { HttpClient } from '@angular/common/http';
+
+
+import { BasicErrorControllerService } from './api/basicErrorController.service';
+import { Cinema1ApiService } from './api/cinema1Api.service';
+import { MetaServiceApiService } from './api/metaServiceApi.service';
+
+@NgModule({
+  imports:      [],
+  declarations: [],
+  exports:      [],
+  providers: [
+    BasicErrorControllerService,
+    Cinema1ApiService,
+    MetaServiceApiService ]
+})
+export class ApiModule {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+        return {
+            ngModule: ApiModule,
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+        };
+    }
+
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
+                 @Optional() http: HttpClient) {
+        if (parentModule) {
+            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+        }
+        if (!http) {
+            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+            'See also https://github.com/angular/angular/issues/20575');
+        }
+    }
+}
