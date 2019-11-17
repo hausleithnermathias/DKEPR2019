775dddac0f5d0fcff1ecb5131524dc24d78cdb9d
11701902 <k11701902@students.jku.at>
2019-11-17 19:02:25 +0100

    cinema1 changes

--- cinema1/src/main/java/at/dkepr/cinemaservice/cinema1/services/Cinema1Service.java
+++ cinema1/src/main/java/at/dkepr/cinemaservice/cinema1/services/Cinema1Service.java
@@ -4,4 +4,6 @@ import org.apache.jena.rdf.model.Model;
 
 public interface Cinema1Service {
     Model getMoviesByDay(String URL, String day);
+    Model getAllMovies(String URL); // hängt sich noch ab und zu auf
+    //Model getAllReservations(String URL);
 }
