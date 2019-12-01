cf17d08ee5873a3eabef7772b2f685931db03743
Felix Baumgärtner <felix.baumgaertner95@gmail.com>
2019-12-01 20:37:27 +0100

    Created Cinema2

---
+++ cinema2/src/main/java/at/dkepr/cinemaservice/cinema2/api/Cinema2API.java
@@ -0,0 +1,53 @@
+package at.dkepr.cinemaservice.cinema2.api;
+
+import at.dkepr.cinemaservice.cinema2.services.Cinema2Service;
+import org.apache.jena.rdf.model.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/Cinema2")
+public class Cinema2API {
+
+    @Autowired
+    Cinema2Service cinema2Service;
+
+    @RequestMapping(value="/Movies/{day}", method= RequestMethod.GET, produces={"application/xml", "application/rdf+xml"})
+    public @ResponseBody String getMoviesByDay(@PathVariable String day) throws IOException {
+        Model model = cinema2Service.getMoviesByDay("http://localhost:3030/cinema2", day);
+        try(final ByteArrayOutputStream os = new ByteArrayOutputStream() ){
+            model.write(os);
+            return os.toString();
+        }
+    }
+
+    @RequestMapping(value="/Movies/All", method= RequestMethod.GET, produces={"application/xml", "application/rdf+xml"})
+    public @ResponseBody String getAllMovies() throws IOException {
+        Model model = cinema2Service.getAllMovies("http://localhost:3030/cinema2");
+        try(final ByteArrayOutputStream os = new ByteArrayOutputStream() ){
+            model.write(os);
+            return os.toString();
+        }
+    }
+
+    @RequestMapping(value="/Movies/Reservations/{name}", method= RequestMethod.GET, produces={"application/xml", "application/rdf+xml"})
+    public @ResponseBody String getReservationName(@PathVariable String name) throws IOException {
+        Model model = cinema2Service.getReservationName("http://localhost:3030/cinema2", name);
+        try(final ByteArrayOutputStream os = new ByteArrayOutputStream() ){
+            model.write(os);
+            return os.toString();
+        }
+    }
+
+    @RequestMapping(value="/Movies/Genre/{genre}", method= RequestMethod.GET, produces={"application/xml", "application/rdf+xml"})
+    public @ResponseBody String getMoviesPerGenre(@PathVariable String genre) throws IOException {
+        Model model = cinema2Service.getMoviesPerGenre("http://localhost:3030/cinema2", genre);
+        try(final ByteArrayOutputStream os = new ByteArrayOutputStream() ){
+            model.write(os);
+            return os.toString();
+        }
+    }
+}
